15f3b9a7d45698d36542d00ad3e5df25c1aad8d7
AnisBouk <anismouhammad@gmail.com>
2024-05-12 18:50:58 +0100

    section 12-13 done

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -4,7 +4,8 @@ class API
   static const hostConnectUser = "$hostConnect/user";
   static const hostConnectAdmin = "$hostConnect/admin";
   static const hostUploadItem = "$hostConnect/items";
-
+  static const hostClothes = "$hostConnect/clothes";
+  //singup-Login user
   static const validateEmail="$hostConnectUser/validate_email.php";
   static const signUp="$hostConnectUser/signup.php";
    static const login="$hostConnectUser/login.php";
@@ -13,4 +14,8 @@ class API
   static const adminLogin="$hostConnectAdmin/login.php";
   //upload-save new item
   static const uploadNewItem="$hostUploadItem/upload.php";
+
+  //Clothes
+  static const getTrendingMostPopularClothes = "$hostClothes/trending.php";
+  static const getAllClothes = "$hostClothes/all.php";
 }
